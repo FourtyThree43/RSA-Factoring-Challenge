96ed37a55aed2216a70e115926970b4bfbdeeb4e
FourtyThree43 <shaqmwa@outlook.com>
2023-04-02 21:28:50 +0300

    Yolo

--- __test__/genPrimes.py
+++ __test__/genPrimes.py
@@ -10,10 +10,10 @@ def is_prime(n):
     return True
 
 def generate_composite_numbers():
-    n = 1000000
+    n = 10000
     composite_numbers = []
     while len(composite_numbers) < n:
-        x = random.randint(2, 10000000)
+        x = random.randint(2, 10**12)
         if not is_prime(x):
             composite_numbers.append(x)
     return composite_numbers
--- __test__/genTestNum.py
+++ __test__/genTestNum.py
@@ -5,8 +5,8 @@ nums = [4, 12, 34, 128, 1024, 4958, 1718944270642558716715,
         9, 99, 999, 9999, 9797973, 49, 239809320265259]
 
 # Generate 100 more random numbers between 10^19 and 10^20
-for i in range(100):
-    n = random.randint(10**19, 10**20)
+for i in range(1000):
+    n = random.randint(2, 10**20)
     nums.append(n)
 
 # Write the numbers to a file
